5f73f0b6b14cb1ee2eb74149a0e2a7fa552317c8
Andy <abaudin@gmail.com>
2015-09-10 18:51:15 +0000

    Validation du submit dans contacts_controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,10 +2,23 @@ class ContactsController < ApplicationController
     
    def new
         @contact = Contact.new
-    end
+   end
    
    def create
+      @contact = Contact.new(contact_params)
+      if @contact.save
+         redirect_to about_path
+      else
+         redirect_to new_contact_path
+      end
        
+   end
+    
+    
+    private
+    
+   def contact_params
+      params.require(:contact).permit(:name, :email, :comments)
    end
     
 end
