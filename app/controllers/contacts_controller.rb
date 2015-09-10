2ab78cbad6404ffa7b7c331524af2bd33523ded5
Andy <abaudin@gmail.com>
2015-09-10 18:54:58 +0000

    Correction de notices dans contacts_controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,7 +9,7 @@ class ContactsController < ApplicationController
       if @contact.save
          redirect_to about_path, notice: "Message envoyé"
       else
-         redirect_to new_contact_path, , notice: "Message non envoyé"
+         redirect_to new_contact_path,  notice: "Message non envoyé"
       end
        
    end
