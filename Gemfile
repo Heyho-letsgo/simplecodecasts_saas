91943cd9dce8ab5a2c8abedaf188b28f3e264af8
Andy <abaudin@gmail.com>
2015-09-11 09:54:43 +0000

    Rajout des validations dans contact.rb

--- Gemfile
+++ Gemfile
@@ -3,6 +3,9 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.0'
+# test de gem table print plutôt bien
+#gem 'table_print'
+
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3', group: [:development, :test]
 
--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,22 +1,24 @@
 class ContactsController < ApplicationController
     
    def new
-        @contact = Contact.new
+      @contact = Contact.new
    end
    
    def create
-      @contact = Contact.new(contact_params)
+   @contact = Contact.new(contact_params)
       if @contact.save
-         redirect_to about_path, notice: "Message envoyé"
+         flash[:success] = "Message Envoyé !"
+         redirect_to new_contact_path
       else
-         redirect_to new_contact_path,  notice: "Message non envoyé"
+         flash[:danger] = "Message non envoyé !"
+         redirect_to new_contact_path
       end
-       
    end
+
     
+   private
     
-    private
-    
+   
    def contact_params
       params.require(:contact).permit(:name, :email, :comments)
    end
